6852339b51144dddb63c3f715252572187f3ac56
Md. Prabej Aalam <np01ai4s260007@islingtoncollege.edu.np>
2026-04-19 19:13:30 +0545

    BankMain.java

--- prabesh homework/BankMain/BankMain.java
+++ prabesh homework/BankMain/BankMain.java
@@ -3,39 +3,39 @@ public class BankMain
     public static void main(String[] args)
     {
         BankAccount x=new BankAccount();
-        x.number="BOA101";
-        x.customer="Hari Prasad";
-        x.money=7500;
+         x.number="BOA101";
+         x.customer="Hari Prasad";
+         x.money=7500;
         
         BankAccount y=new BankAccount();
-        y.number="BOA102";
-        y.customer="Gita Kumari";
-        y.money=12000;
+         y.number="BOA102";
+         y.customer="Gita Kumari";
+         y.money=12000;
 
         System.out.println(">> acc 1 details");
-        x.showBal();
+         x.showBal();
 
         System.out.println("");
 
         System.out.println(">> acc 2 details");
-        y.showBal();
+         y.showBal();
 
         System.out.println("");
         
         System.out.println(">> transactions on acc 1");
-        x.deposit(3000);
-        x.withdraw(1500);
+         x.deposit(3000);
+         x.withdraw(1500);
 
         System.out.println("");
 
         System.out.println(">> transactions on acc 2");
-        y.deposit(4000);
-        y.withdraw(2500);
+         y.deposit(4000);
+         y.withdraw(2500);
 
         System.out.println("");
         System.out.println("## FINAL BALANCE ##");
-        x.showBal();
+         x.showBal();
         System.out.println("");
-        y.showBal();
+         y.showBal();
     }
 }
